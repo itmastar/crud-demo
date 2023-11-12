1e125aaf61292c1f9950a4b2fb2667c204f43e47
Sravani <sravanilal1986@gmail.com>
2023-11-12 08:31:05 +0530

    First Commit new1

--- src/main/java/com/itmastar/crud/entity/Worker.java
+++ src/main/java/com/itmastar/crud/entity/Worker.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Worker {
-
+//Test Comment
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
